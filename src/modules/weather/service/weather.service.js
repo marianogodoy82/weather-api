e49b627fd58f311942f13cd4109ca710ede4b583
Mariano Godoy <aramismdg@gmail.com>
2021-10-26 10:25:06 -0300

    delete log

--- src/modules/weather/service/weather.service.js
+++ src/modules/weather/service/weather.service.js
@@ -2,7 +2,6 @@ import fetch from 'node-fetch'
 
 async function fetchCurrent(city) {
         const URI = `${process.env.openWeatherEndpoint}/weather?q=${city}&appid=${process.env.openWeatherApiKey}&units=metric`;
-        console.log(URI)
         try{
             const response = await fetch(URI);
             return await response.json();
